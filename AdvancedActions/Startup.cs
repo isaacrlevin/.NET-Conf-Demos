b42ca5ed06e721e871b369868b960a2e60bf6214
Isaac Levin <isaac.r.levin@gmail.com>
2021-11-08 08:59:22 -0800

    Updates

--- AdvancedActions/Startup.cs
+++ AdvancedActions/Startup.cs
@@ -17,7 +17,6 @@ namespace AdvancedActions
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
